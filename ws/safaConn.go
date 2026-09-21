0f448db74e4be6dfd98bd867e99900b4668c1034
wander44-svg <huangzk44@gmail.com>
2026-09-22 06:58:36 +0800

    fix: expose websocket read limit

--- ws/safaConn.go
+++ ws/safaConn.go
@@ -51,6 +51,9 @@ func (sc *SafeConn) SetReadDeadline(t time.Time) error {
 	// defer sc.mu.Unlock()
 	return sc.conn.SetReadDeadline(t)
 }
+func (sc *SafeConn) SetReadLimit(limit int64) {
+	sc.conn.SetReadLimit(limit)
+}
 func (sc *SafeConn) GetConn() *websocket.Conn {
 	sc.mu.Lock()
 	defer sc.mu.Unlock()
